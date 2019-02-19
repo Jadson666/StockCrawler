94292241d4a515d7110d4459ecc255654f5966d6
Jason Lin <amigo_brian@hotmail.com>
2019-02-19 18:21:37 +0800

    change to fetch-node version

--- PriceService.js
+++ PriceService.js
@@ -1,13 +1,57 @@
 const fakeHeader = require('./config').fakeHeader;
+const cheerio = require("cheerio");
+const fs = require("fs");
+const fetch = require("node-fetch");
+
+let number;
+let result;
+
+async function getPriceData(stockNumber){
+  number = stockNumber;
+  const response = await fetch(getParameter().url, getParameter().headers);
+  result = await getData(response);
+  return result;
+}
 
 function getParameter() {
   return {
     headers: fakeHeader,
-    url: "https://goodinfo.tw/StockInfo/StockDetail.asp?STOCK_ID=1560",
+    url: "https://goodinfo.tw/StockInfo/StockDetail.asp?STOCK_ID=" + number,
     method: "GET"
   };
 }
 
+async function getData(response) {
+  if (!response.ok) {
+    console.log("some error happen: " + response.statusText);
+    return;
+  }
+  body = await response.text();
+  const $ = cheerio.load(body); // 載入 body
+  const table_tr = $(".std_tbl tr"); // 爬最外層的 Table(class=BoxTable) 中的 tr
+  const table_td = table_tr.eq(3).find("td");
+  const price = table_td.eq(0).text();
+  const changePrice = table_td.eq(1).text();
+  const changeRate = table_td.eq(2).text();
+  const priceYesterday = table_td.eq(3).text();
+  const openPrice = table_td.eq(4).text();
+  const highestPrice = table_td.eq(5).text();
+  const lowestPrice = table_td.eq(6).text();
+  
+  result = {
+    price,
+    changePrice,
+    changeRate,
+    priceYesterday,
+    openPrice,
+    highestPrice,
+    lowestPrice
+  }
+  // 寫入 result.json 檔案
+  fs.writeFileSync("result.json", JSON.stringify(result));
+  return result;
+}
+
 module.exports = {
-  getRequestParameter: getParameter
+  getPriceData
 };
--- crawler.js
+++ crawler.js
@@ -1,43 +1,16 @@
-const request = require("request");
-const cheerio = require("cheerio");
-const fs = require("fs");
 const priceService = require("./PriceService");
 
-(function startToCraw() {
-  request(
-    priceService.getRequestParameter(),
-    function(error, response, body) {
-      if (error || !body) {
-        console.log("some error happen: " + error);
-        return;
-      }
-      const $ = cheerio.load(body); // 載入 body
-      const result = []; // 建立一個儲存結果的容器
-      const table_tr = $(".std_tbl tr"); // 爬最外層的 Table(class=BoxTable) 中的 tr
-      // 建立物件並(push)存入結果
-      const table_td = table_tr.eq(3).find("td");
-      const price = table_td.eq(0).text();
-      const changePrice = table_td.eq(1).text();
-      const changeRate = table_td.eq(2).text();
-      const priceYesterday = table_td.eq(3).text();
-      const openPrice = table_td.eq(4).text();
-      const highestPrice = table_td.eq(5).text();
-      const lowestPrice = table_td.eq(6).text();
-      result.push(
-        Object.assign({
-          price,
-          changePrice,
-          changeRate,
-          priceYesterday,
-          openPrice,
-          highestPrice,
-          lowestPrice
-        })
-      );
-      console.log(result);
-      // 寫入 result.json 檔案
-      fs.writeFileSync("result.json", JSON.stringify(result));
-    }
-  );
+(async function startToCraw() {
+  const stockNo = 3406;
+  const priceObject = await priceService.getPriceData(stockNo);
+  let {price, changePrice, changeRate} = priceObject
+  console.log(
+    `
+     股票編號: ${stockNo}
+     價錢: ${price}
+     漲跌幅: ${changePrice}
+     漲跌率: ${changeRate}
+    `
+  )
 })();
 
